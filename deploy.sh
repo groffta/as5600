74ba9e3e659b8186de1a1730355de0143caede27
Todd <Todd>
2022-02-07 20:29:35 -0500

    update deploy script to make local lib dir

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 CRATE_ROOT=$(dirname $0)
 
 cargo build --target armv7-unknown-linux-gnueabihf
+ssh bb "mkdir -p ~/.local/lib/"
 scp $CRATE_ROOT/target/armv7-unknown-linux-gnueabihf/debug/libas5600.so bb:~/.local/lib/libas5600.so
 scp $CRATE_ROOT/as5600.py bb:~/as5600.py
 ssh bb "python3 ~/as5600.py"
